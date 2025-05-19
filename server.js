b9c12900c8fe6bd660241ca92f2619a066c6d6dc
vitor.ferreira <vitor.ferreira@capgemini.com>
2025-05-19 17:16:22 -0300

    feat: add server production rules

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ import userRouter from './routes/userRouter.js';
 import billRouter from './routes/billRouter.js';
 import cardRouter from './routes/cardRouter.js';
 import mongoose from 'mongoose';
-import cors from 'cors'
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3005;
@@ -17,6 +22,9 @@ app.options("", cors());
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
+} else {
+    // Serve os arquivos estáticos do React
+    app.use(express.static(path.join(__dirname, 'client/dist')));
 }
 
 app.use(express.json());
