9820fe1709a9ca3ecb9692b5b9d64e46b388582b
vitor.ferreira <vitor.ferreira@capgemini.com>
2025-05-22 19:19:34 -0300

    fix: remove test console

--- client/src/store/apis/usersApi.js
+++ client/src/store/apis/usersApi.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const baseUrl = import.meta.env.MODE === 'production' ? import.meta.VITE_PROD_URL : `${location.protocol}//${location.hostname}:3005/api/v1`;
-
-console.log('baseUrl', baseUrl);
+const baseUrl = import.meta.env.MODE === 'production' ? import.meta.env.VITE_PROD_URL : `${location.protocol}//${location.hostname}:3005/api/v1`;
 
 const api = axios.create({ baseURL: baseUrl, /* headers : {'Authorization' : `Bearer ${apiToken}`} */ });
 
